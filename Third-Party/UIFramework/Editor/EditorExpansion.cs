3d9cf1c6c410e537fe19aa16634231b2e16c4775
Lowy <1428416750@qq.com>
2019-09-08 00:02:26 +0800

    修改结构

---
+++ Third-Party/UIFramework/Editor/EditorExpansion.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lowy.UIFramework
+{
+    public class WindowContent
+    {
+        public static string[] UIView_base_Names = new[] {"UIView", "AnimUIView"};
+        public string cs_name = "NewUIView";
+        public UIContentType content_type = UIContentType.UIView;
+        public int uiView_base = 0;
+        public Vector2 screenSize = new Vector2(1080, 1920);
+        public string cs_path = "Scripts/View";
+        public string prefab_res_path = "Resources/View";
+    }
+
+    public class CreateViewWindow : EditorWindow
+    {
+        private WindowContent _content;
+        private bool _createing;
+
+        [MenuItem("Window/UIFramework/Create View %u")]
+        public static void CreateView()
+        {
+            //创建窗口
+            CreateViewWindow window = GetWindow();
+
+            window.Show();
+        }
+
+        public static CreateViewWindow GetWindow()
+        {
+            Rect wr = new Rect(0, 0, 500, 350);
+            return (CreateViewWindow) EditorWindow.GetWindowWithRect(typeof(CreateViewWindow), wr, true,
+                "Create View Window");
+        }
+
+        private void OnEnable()
+        {
+            _content = GetContent();
+        }
+
+        private void OnDisable()
+        {
+            SaveContent(_content);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(20);
+            if (_createing)
+            {
+                EditorGUILayout.LabelField("Create UIViewMono OK");
+                EditorGUILayout.LabelField("Loading Script ...");
+                EditorGUILayout.LabelField("Create Prefabs wait");
+                return;
+            }
+
+            _content.cs_name = EditorGUILayout.TextField("CS name", _content.cs_name).Trim();
+            _content.content_type = (UIContentType) EditorGUILayout.EnumPopup("Content Type", _content.content_type);
+            _content.uiView_base =
+                EditorGUILayout.Popup("UIView Base Class", _content.uiView_base, WindowContent.UIView_base_Names);
+            _content.screenSize = EditorGUILayout.Vector2Field("Screen Size：", _content.screenSize);
+            GUILayout.Space(20);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Select Cs Path", GUILayout.MaxWidth(150)))
+            {
+                string path = SelectPath();
+                if (!string.IsNullOrEmpty(path) && path.Contains(Application.dataPath+ "/"))
+                {
+                    _content.cs_path = path.Replace(Application.dataPath + "/", "");
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("UI Framework", $"'{path}' Path invalid, path need has '{Application.dataPath}/'", "ok");
+                }
+            }
+
+            GUILayout.Label("CS Path：" + _content.cs_path);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Select Resources Path", GUILayout.MaxWidth(150)))
+            {
+                string path = SelectPath();
+                if (!string.IsNullOrEmpty(path) && path.Contains(Application.dataPath+ "/"))
+                {
+                    _content.prefab_res_path = path.Replace(Application.dataPath + "/", "");
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("UI Framework", $"'{path}' Path invalid, path need has '{Application.dataPath}/'", "ok");
+                }
+            }
+
+            GUILayout.Label("CS Path：" + _content.prefab_res_path);
+            EditorGUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Create Prefabs", GUILayout.Height(40)))
+            {
+                SaveContent(_content);
+                if (CanCreate(_content))
+                {
+                    CreateMonoUIView(_content);
+                    _createing = true;
+                }
+                else
+                {
+                    EditorUtility.DisplayDialog("提示", "文件已存在", "ok");
+                    _createing = false;
+                }
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+
+            EditorGUILayout.LabelField("如果修改视图类型：");
+            EditorGUILayout.LabelField("1.代码'重命名'脚本名，后缀为所需的上下文类型");
+            EditorGUILayout.LabelField("2.下面的上下文中修改类型为对应的枚举");
+            EditorGUILayout.LabelField("3.重命名文件，保持与脚本对应");
+            EditorGUILayout.LabelField("4.重命名预制物，与脚本对应");
+            EditorGUILayout.LabelField("5.移动预制物到相应的文件夹中");
+        }
+
+        private bool CanCreate(WindowContent content)
+        {
+            if (File.Exists($"{Application.dataPath}/{content.cs_path}/{content.cs_name}{content.content_type}.cs"))
+                return false;
+            if (File.Exists(
+                $"{Application.dataPath}/{content.prefab_res_path}/{content.content_type}/{content.cs_name}{content.content_type}.prefab")
+            )
+                return false;
+            return true;
+        }
+
+        private string SelectPath()
+        {
+            return EditorUtility.SaveFolderPanel("Select cs save path", Application.dataPath, "Assets");
+        }
+
+        private WindowContent GetContent()
+        {
+            if (PlayerPrefs.HasKey("UI_FRAMEWORK_WINDOW_CONTENT_KEY"))
+                return JsonUtility.FromJson<WindowContent>(PlayerPrefs.GetString("UI_FRAMEWORK_WINDOW_CONTENT_KEY"));
+            return new WindowContent();
+        }
+
+        private void SaveContent(WindowContent content)
+        {
+            PlayerPrefs.SetString("UI_FRAMEWORK_WINDOW_CONTENT_KEY", JsonUtility.ToJson(content));
+        }
+
+        private void CreateMonoUIView(WindowContent content)
+        {
+            string path = $"{Application.dataPath}/{content.cs_path}";
+            CreatePath(path);
+            string oldText = "";
+            if (File.Exists($"{path}/{content.cs_name}{content.content_type}.cs"))
+                oldText = File.ReadAllText($"{path}/{content.cs_name}{content.content_type}.cs");
+            string text = Resources.Load<TextAsset>("UIFramework/UIView.cs").text;
+            text = text.Replace("$FILE_NAME$", content.cs_name);
+            text = text.Replace("$CONTENT_TYPE$", content.content_type.ToString());
+            text = text.Replace("$UI_VIEW$", WindowContent.UIView_base_Names[content.uiView_base]);
+            //
+            if (text == oldText)
+            {
+                _createing = false;
+                return;
+            }
+
+            //
+            File.WriteAllText($"{path}/{content.cs_name}{content.content_type}.cs",
+                text);
+        }
+
+        private void CreatePrefabs(WindowContent content)
+        {
+            string name = $"{content.cs_name}{content.content_type}";
+            var g = new GameObject(name);
+            CreateCanvas(g, _content);
+            RectTransform rect = new GameObject("Root", typeof(Image)).transform as RectTransform;
+            rect.transform.SetParent(g.transform);
+            rect.anchorMin = Vector2.zero;
+            rect.anchorMax = Vector2.one;
+            rect.sizeDelta = Vector2.zero;
+            rect.anchoredPosition = Vector2.zero;
+            rect.GetComponent<Image>().color = new Color(1, 1, 1, 0.4f);
+            rect.gameObject.layer = LayerMask.NameToLayer("UI");
+            //
+            CreatePath($"{Application.dataPath}/{content.prefab_res_path}/{content.content_type}");
+            //
+            PrefabUtility.SaveAsPrefabAsset(g,
+                $"Assets/{content.prefab_res_path}/{content.content_type}/{name}.prefab");
+            DestroyImmediate(g);
+        }
+
+        [UnityEditor.Callbacks.DidReloadScripts]
+        static void AllScriptsReloaded()
+        {
+            var window = GetWindow();
+            if (!window._createing)
+            {
+                window.Close();
+                return;
+            }
+
+            window.CreatePrefabs(window._content);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            window._createing = false;
+        }
+
+        private void CreatePath(string s)
+        {
+            if (!Directory.Exists(s))
+                Directory.CreateDirectory(s);
+        }
+
+        private void CreateCanvas(GameObject g, WindowContent conotent)
+        {
+            g.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
+            var scaler = g.AddComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.referenceResolution = conotent.screenSize;
+            g.AddComponent<GraphicRaycaster>();
+            var t = Assembly.Load("Assembly-CSharp").GetType(conotent.cs_name + conotent.content_type);
+            g.AddComponent(t);
+            g.layer = LayerMask.NameToLayer("UI");
+        }
+    }
+}
---
+++ Third-Party/UIFramework/Scripts/UIManager.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Lowy.Bind;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Object = UnityEngine.Object;
+
+namespace Lowy.UIFramework
+{
+    /// <summary>
+    /// UI管理
+    /// </summary>
+    public class UIManager
+    {
+        private static UIManager _ins;
+        public static UIManager Ins
+        {
+            get
+            {
+                if (_ins == null) _ins = new UIManager();
+                return _ins;
+            }
+        }
+
+        private UIFrameworkConfig _config;
+
+        private UIFrameworkConfig Config
+        {
+            get
+            {
+                if (_config == null)
+                    _config = Resources.Load<UIFrameworkConfig>("UIFramework/Config");
+                return _config;
+            }
+        }
+        
+        private Transform _UIRoot;
+
+        private Camera _uiCamera;
+
+        //ui
+        private Dictionary<Type, UIView> _uiDic;
+
+        //stack
+        private List<AbsContent> _stack;
+
+        public UIManager()
+        {
+            _uiDic = new Dictionary<Type, UIView>();
+            _stack = new List<AbsContent>();
+            if (_UIRoot == null)
+            {
+                _UIRoot = new GameObject("UIRoot").transform;
+                Object.DontDestroyOnLoad(_UIRoot);
+            }
+
+            EventSystem es = Object.FindObjectOfType<EventSystem>();
+            if (es == null)
+                es = new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule))
+                    .GetComponent<EventSystem>();
+            es.transform.SetParent(_UIRoot);
+        }
+        /// <summary>
+        /// 显示窗口，使用此上下文
+        /// </summary>
+        /// <param name="content">上下文</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static UIView Push<T>([NotNull] T content) where T : AbsContent
+        {
+            return Ins.Display(content);
+        }
+        /// <summary>
+        /// 关闭一个窗口
+        /// </summary>
+        /// <param name="content">上下文，关闭这个上下文的窗口，如果下一个仍是不同上下问的同一个窗口，将会直接调用<see cref="UIView.ForcedDisable"/>函数，并直接调用下面的上下文的窗口的<see cref="UIView.Resume"/>操作</param>
+        /// <param name="onlyTop">当<see cref="content"/>为null时，是否进关闭最上面的上下文的窗口，如果有多个上下文的同一窗口，则会保留其他的上下文之后进行<see cref="UIView.Resume"/>操作</param>
+        /// <typeparam name="T">上下文类型</typeparam>
+        public static void Pop<T>([CanBeNull] T content = null, bool onlyTop = true) where T : AbsContent
+        {
+            Ins.Disable(content, onlyTop);
+        }
+        /// <summary>
+        /// 禁用所有窗口
+        /// </summary>
+        /// <param name="ignores">忽略列表</param>
+        public static void PopAll(params Type[] ignores)
+        {
+            Ins.DisableAll(ignores);
+        }
+        /// <summary>
+        /// 查找正在显示的视图
+        /// </summary>
+        /// <typeparam name="T">视图对应的上下文类型</typeparam>
+        /// <returns></returns>
+        public UIView FindDisplayUIView<T>() where T : AbsContent
+        {
+            var view = FindUIView<T>(null);
+            return view.gameObject.activeSelf ? view : null;
+        }
+        /// <summary>
+        /// 查找一个视图，如果没有就创建
+        /// </summary>
+        /// <param name="content">上下文，如果为空，则不会创建，并返回null</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private UIView FindUIView<T>(T content) where T : AbsContent
+        {
+            Type t = typeof(T);
+            if (content != null)
+                t = content.GetType();
+            if (_uiDic.ContainsKey(t))
+                return _uiDic[t];
+            if (content == null)
+                return null;
+            var obj = Resources.Load<GameObject>(content.ViewResPath);
+            //TODO 解除绑定
+//            Binder.GetBind(obj.GetComponent<UIView>().GetType()).UnBind();
+            var res = Object.Instantiate(obj, _UIRoot).GetComponent<UIView>();
+            res.name = content.UIViewName();
+            res.Canvas.worldCamera = GetUICamera();
+            int layer = content.ContentType <= UIContentType.UIView ? GetOutUILayer() : GetUILayer();
+            foreach (var child in res.GetComponentsInChildren<Transform>())
+                child.gameObject.layer = (int) Mathf.Log(layer,2);
+            //
+            _uiDic.Add(t, res);
+            return res;
+        }
+        /// <summary>
+        /// 重设UI的渲染顺序
+        /// </summary>
+        private void RefreshOrder()
+        {
+            int i = 10;
+            foreach (var content in _stack)
+            {
+                FindUIView(content).Canvas.sortingOrder = i++;
+            }
+        }
+        /// <summary>
+        /// 将上下文入栈，但是会受到<see cref="AbsContent.Priority"/>的优先级限制
+        /// </summary>
+        /// <param name="content"></param>
+        private void PushStack([NotNull] AbsContent content)
+        {
+            //如果有覆盖层则剔除这个层
+            if (content.Priority == UIContentType.OverView.ValueToInt()&&
+                (GetUICamera().cullingMask |= GetOutUILayer())==GetUICamera().cullingMask)
+                GetUICamera().cullingMask ^= GetOutUILayer();
+            if (_stack.Count <= 0)
+            {
+                _stack.Add(content);
+                return;
+            }
+
+            for (int i = _stack.Count - 1; i >= 0; i--)
+            {
+                if (_stack[i].Priority <= content.Priority)
+                {
+                    if (i == _stack.Count - 1)
+                        _stack.Add(content);
+                    else
+                        _stack.Insert(i + 1, content);
+                    return;
+                }
+            }
+
+            _stack.Insert(0, content);
+        }
+
+        private void PopStack([NotNull] AbsContent content)
+        {
+            _stack.Remove(content);
+            //如果没有覆盖层则增加显示剔除层
+            if (content.Priority == UIContentType.OverView.ValueToInt())
+            {
+                if (_stack.Find(c => c.Priority == UIContentType.OverView.ValueToInt()) == null&&
+                    (GetUICamera().cullingMask |= GetOutUILayer())==GetUICamera().cullingMask)
+                    GetUICamera().cullingMask ^= GetOutUILayer();
+            }
+        }
+
+        /// <summary>
+        /// 展示一个窗口
+        /// </summary>
+        /// <param name="content"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private UIView Display<T>([NotNull] T content) where T : AbsContent
+        {
+            var t = typeof(T);
+            var view = FindUIView(content);
+            PushStack(content);
+            RefreshOrder();
+            view.Display(content);
+            if (_stack.Last() == content)
+            {
+                if (_stack.Count > 1)
+                    FindUIView(_stack[_stack.Count - 2]).Pause(_stack[_stack.Count - 2]);
+            }
+            else
+            {
+                view.Pause(content);
+            }
+
+            return _uiDic[t];
+        }
+
+        /// <summary>
+        /// 关闭一个窗口，上下文可为null，此时将依据<see cref="onlyTop"/>禁用
+        /// </summary>
+        /// <param name="content">上下文，关闭这个上下文的窗口，如果下一个仍是不同上下问的同一个窗口，将会直接调用<see cref="UIView.ForcedDisable"/>函数，并直接调用下面的上下文的窗口的<see cref="UIView.Resume"/>操作</param>
+        /// <param name="onlyTop">当<see cref="content"/>为null时，是否进关闭最上面的上下文的窗口，如果有多个上下文的同一窗口，则会保留其他的上下文之后进行<see cref="UIView.Resume"/>操作</param>
+        /// <typeparam name="T">上下文类型</typeparam>
+        private void Disable<T>([CanBeNull] T content = null, bool onlyTop = true) where T : AbsContent
+        {
+            var t = typeof(T);
+            var view = FindUIView(content);
+            if (content != null)
+            {
+                DisableSelf(content);
+            }
+            else
+            {
+                var lastCon = _stack.Last();
+                for (int i = _stack.Count - 1; i >= 0; i--)
+                {
+                    if (_stack[i].GetType() == t)
+                    {
+                        if (onlyTop)
+                        {
+                            DisableSelf(_stack[i]);
+                            RefreshOrder();
+                            return;
+                        }
+
+                        DisableSelf(_stack[i], false);
+                    }
+                }
+
+                if (lastCon != _stack.Last() && _stack.Count > 0)
+                    FindUIView(_stack.Last()).Resume(_stack.Last());
+            }
+
+            RefreshOrder();
+        }
+        /// <summary>
+        /// 关闭一个窗口，强烈依赖上下文，不可为null
+        /// </summary>
+        /// <param name="content">要关闭的窗口的上下文</param>
+        /// <param name="needResume"></param>
+        /// <typeparam name="T"></typeparam>
+        private void DisableSelf<T>([NotNull] T content, bool needResume = true) where T : AbsContent
+        {
+            var t = typeof(T);
+            var view = FindUIView(content);
+            bool top = _stack.Last() == content;
+            //如果在栈顶
+            if (top)
+            {
+                if (_stack.Count > 1)
+                {
+                    if (_stack[_stack.Count - 2].GetType() == t)
+                        view.ForcedDisable(content);
+                    else
+                        view.Disable(content);
+                    //
+                    PopStack(content);
+                    if (needResume)
+                        FindUIView(_stack.Last()).Resume(_stack.Last());
+                    return;
+                }
+            }
+
+            view.Disable(_stack[_stack.Count - 1]);
+            PopStack(_stack[_stack.Count - 1]);
+            return;
+        }
+
+        /// <summary>
+        /// 禁用所有窗口
+        /// </summary>
+        /// <param name="ignores">忽略部分Content</param>
+        private void DisableAll(params Type[] ignores)
+        {
+            if (_stack.Count <= 0)
+                return;
+            var lastCon = _stack[_stack.Count - 1];
+            for (int i = _stack.Count - 1; i >= 0; i--)
+            {
+                if (ignores != null && ignores.Contains(_stack[i].GetType()))
+                    continue;
+                DisableSelf(_stack[i], false);
+            }
+
+            if (_stack.Count > 0 && lastCon != _stack.Last() )
+                FindUIView(_stack.Last()).Resume(_stack.Last());
+            RefreshOrder();
+        }
+        /// <summary>
+        /// 获取UICamera
+        /// </summary>
+        /// <returns></returns>
+        public Camera GetUICamera()
+        {
+            if (_uiCamera == null)
+            {
+                var camera = GenerateUICamera();
+                //
+                camera.transform.SetParent(_UIRoot);
+                camera.transform.SetSiblingIndex(0);
+                _uiCamera = camera.GetComponent<Camera>();
+            }
+
+            return _uiCamera;
+        }
+
+        public int GetUILayer()
+        {
+            return Config.UILayer;
+        }
+
+        public int GetOutUILayer()
+        {
+            return Config.OutUILayer;
+        }
+
+        public int GetUIParticleLayer()
+        {
+            return Config.UIParticleLayer;
+        }
+
+        private GameObject GenerateUICamera()
+        {
+            var camera = new GameObject("UICamera", typeof(Camera)).GetComponent<Camera>();
+            camera.clearFlags = CameraClearFlags.Depth;
+            camera.cullingMask = GetUILayer();
+            camera.orthographic = true;
+            camera.orthographicSize = 5;
+            camera.depth = 1;
+            //
+            var particleCamera = new GameObject("ParticleCamera",typeof(Camera)).GetComponent<Camera>();
+            particleCamera.clearFlags = CameraClearFlags.Depth;
+            particleCamera.cullingMask = GetUIParticleLayer();
+            particleCamera.orthographic = false;
+            particleCamera.fieldOfView = 60;
+            particleCamera.depth = 1;
+            particleCamera.transform.SetParent(camera.transform);
+            return camera.gameObject;
+        }
+    }
+}
