018e3716b2b6f68e90ec85639100af798c922dd4
Lowy <1428416750@qq.com>
2019-09-08 00:42:38 +0800

    增加package,增加asmdef,版本1.0.1

--- Test/Test.cs
+++ Test/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UIManager.Push(new NewUIViewContent());
+        
     }
 
     // Update is called once per frame
